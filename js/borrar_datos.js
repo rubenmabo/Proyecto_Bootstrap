287030989da6f1ec098bf402da380662f23bb1e9
rubenmabo <47483262+rubenmabo@users.noreply.github.com>
2020-02-19 04:44:19 +0100

    Add files via upload

---
+++ js/borrar_datos.js
@@ -0,0 +1,15 @@
+var x;
+x=$(document);
+x.ready(fBoton1);
+
+function fBoton1()
+{
+  var x=$("#boton1");
+  x.click(borrarTabla);
+}
+
+function borrarTabla()
+{
+  var x=$("#tabla1 td.p");
+  x.text("-");
+}
